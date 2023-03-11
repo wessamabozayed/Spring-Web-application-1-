42bf7277012af9e2a58032ffc99d102eb02ae365
wessamabozayed <wessam.abozayed@gmail.com>
2023-03-11 14:50:45 +0400

    first commit

---
+++ src/test/java/wessam/spring/springWebApp/SpringWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package wessam.spring.springWebApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
